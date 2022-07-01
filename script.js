9aee60ce2c5660b7c331176a21fc0481765c652a
Muhammad Usman <musmanraodev@gmail.com>
2022-07-01 13:02:40 -0400

    updated card

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-const board = document.getElementById("game-board");
-const rope = document.querySelector(".rope");
-const restartBttn = document.querySelector("#bttn");
-const game = document.getElementById("game");
-const instructions = document.getElementById("instructions");
-const navigationBarItems = [...document.querySelectorAll('.header > a')];
-const contentChildren = [...document.querySelectorAll('.content > div')];
-
-navigationBarItems.forEach((item, i) => {
-    item.addEventListener("click", () => {
-        contentChildren.forEach(content => content.classList.add('hide'));
-        contentChildren[i].classList.remove('hide');
-    });
-})
-
 const cardsValueArr = ['apple', 'earth', 'flower', 'house', 'water'];
 
 class Card {
-    constructor(board, howManyCardsToCreate) {
+    constructor(board, numberOfCardsToCreate) {
         this.board = board;
-        this.howManyCardsToCreate = howManyCardsToCreate;
+        this.numberOfCardsToCreate = numberOfCardsToCreate;
         this.cardsInPlay = [];
         this.flipallow = true;
         this.cardsSetFounded = 0;
+        this.score = document.querySelector(".score");
+        this.rope = document.querySelector(".rope");
+        this.restartBttn = document.querySelector("#bttn");
     }
 
     createCardHtml(value) {
-        let container = document.createElement('div');
+        const container = document.createElement('div');
         container.className = 'container options';
 
-        let card = document.createElement('div');
-        card.id = 'card';
+        const card = document.createElement('div');
+        card.className = 'card';
         container.appendChild(card);
 
-        let front = document.createElement('figure');
+        const front = document.createElement('figure');
         front.className = 'front';
 
-        let back = document.createElement('figure');
+        const back = document.createElement('figure');
         back.className = 'back';
 
         // TODO: fix this
@@ -45,76 +33,95 @@ class Card {
         card.appendChild(back);
 
         container.setAttribute('data-card', value);
-        container.setAttribute('count', 0);
         return container;
     }
 
+    addLogicToNavigationBar() {
+        const navigationBarItems = [...document.querySelectorAll('.header > a')];
+        const contentChildren = [...document.querySelectorAll('.content > div')];
+
+        navigationBarItems.forEach((item, i) => {
+            item.addEventListener("click", () => {
+                contentChildren.forEach(content => content.classList.add('hide'));
+                contentChildren[i].classList.remove('hide');
+            });
+        })
+    }
+
+    addLogicToRestartBttn() {
+        this.restartBttn.addEventListener('click', () => window.location.reload());
+    }
+
     addLogicToCards() {
+        const playerWonTheGame = () => {
+            [...this.board.children].forEach(card => {
+                card.classList.remove('options');
+                card.classList.add('tada');
+            });
+            this.board.classList.add('fadeOut');
+
+            setTimeout(() => {
+                this.board.classList.add("hide");
+                this.restartBttn.classList.remove("hide");
+                this.rope.classList.add('middle');
+            }, 4500);
+        }
+
+        const changeScore = (value) => (this.score.innerHTML = Number(this.score.innerHTML) + value);
+
+        const handleWhenBothCardsAreSame = () => {
+            setTimeout(() => {
+                changeScore(2);
+
+                this.cardsInPlay[0].setAttribute("alreadyFounded", "true");
+                this.cardsInPlay[0].querySelector('.back').style.backgroundImage = "url('images/won.jpg')";
+
+                this.cardsInPlay[1].setAttribute("alreadyFounded", "true");
+                this.cardsInPlay[1].querySelector('.back').style.backgroundImage = "url('images/won.jpg')";
+
+                this.cardsInPlay = [];
+                this.flipallow = true;
+            }, 1000);
+
+            this.cardsSetFounded++;
+
+            const hasPlayerWonTheGame = this.cardsSetFounded == this.numberOfCardsToCreate;
+            if (hasPlayerWonTheGame) playerWonTheGame();
+        }
+
+        const handleWhenBothCardsAreNotSame = () => {
+            setTimeout(() => {
+                changeScore(-1);
+
+                this.cardsInPlay[0].querySelector('.card').classList.remove("flippedfront");
+                this.cardsInPlay[1].querySelector('.card').classList.remove("flippedfront");
+
+                this.cardsInPlay = [];
+                this.flipallow = true;
+            }, 1000);
+        }
+
         this.board.addEventListener('click', (event) => {
-            const card = event.target.closest('.container');
-            if (!card) return;
+            const cardContainer = event.target.closest('.container');
+            if (!cardContainer) return;
 
-            if (card.hasAttribute('alreadyFounded') || !this.flipallow || card.querySelector('#card').classList.contains('flippedfront')) return;
-            card.firstChild.className = "flipped flippedfront";
-            this.cardsInPlay.push(card);
+            const card = cardContainer.querySelector('.card');
+            if (cardContainer.hasAttribute('alreadyFounded') || !this.flipallow || card.classList.contains('flippedfront')) return;
+            card.classList.add("flippedfront");
+            this.cardsInPlay.push(cardContainer);
 
-            let score = document.querySelector(".score");
             if (this.cardsInPlay.length === 2) {
                 this.flipallow = false;
 
-                if (this.cardsInPlay[0].getAttribute('data-card') == this.cardsInPlay[1].getAttribute('data-card')) {
-                    this.cardsSetFounded++;
-
-                    setTimeout(() => {
-                        score.innerHTML = Number(score.innerHTML) + 2;
-
-                        this.cardsInPlay[0].setAttribute("alreadyFounded", "true");
-                        this.cardsInPlay[0].querySelector('.back').style.backgroundImage = "url('images/won.jpg')";
-
-                        this.cardsInPlay[1].setAttribute("alreadyFounded", "true");
-                        this.cardsInPlay[1].querySelector('.back').style.backgroundImage = "url('images/won.jpg')";
-
-                        this.cardsInPlay = [];
-                        this.flipallow = true;
-                    }, 1000);
-
-                    // Won the game
-                    if (this.cardsSetFounded == this.howManyCardsToCreate) {
-                        [...this.board.children].forEach(card => {
-                            card.classList.remove('options');
-                            card.classList.add('tada');
-                        });
-                        this.board.classList.add('fadeOut');
-
-                        setTimeout(() => {
-                            this.board.style.display = 'none';
-                            restartBttn.classList.remove("hide");
-                            rope.classList.add('middle');
-                        }, 4500);
-                    }
-
-                } else {
-                    score.innerHTML = Number(score.innerHTML) - 1;
-
-                    setTimeout(() => {
-                        this.cardsInPlay[0].querySelector('#card').classList.remove("flippedfront");
-                        this.cardsInPlay[1].querySelector('#card').classList.remove("flippedfront");
-
-                        this.cardsInPlay = [];
-                        this.flipallow = true;
-                    }, 1000);
-
-                }
-
+                const areBothCardsSame = this.cardsInPlay[0].getAttribute('data-card') == this.cardsInPlay[1].getAttribute('data-card');
+                areBothCardsSame ? handleWhenBothCardsAreSame() : handleWhenBothCardsAreNotSame();
             }
         })
-
-
     }
 
     addCardsToBoard() {
         const cardsArr = [];
-        for (let i = 0; i < this.howManyCardsToCreate; i++) {
+        for (let i = 0; i < this.numberOfCardsToCreate; i++) {
             cardsArr.push(this.createCardHtml(i), this.createCardHtml(i))
         }
 
@@ -131,9 +138,12 @@ class Card {
     }
 
     start() {
+        this.addLogicToNavigationBar();
         this.addCardsToBoard();
+        this.addLogicToRestartBttn();
     }
 }
 
+const board = document.getElementById("game-board");
 const cards = new Card(board, 5);
 cards.start();
